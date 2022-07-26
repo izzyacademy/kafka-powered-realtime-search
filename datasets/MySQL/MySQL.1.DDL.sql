07d02dc43a40ee5a2864823e0a26d5aa4b2e2eae
Israel Ekpo <44282278+izzyacademy@users.noreply.github.com>
2022-07-26 15:54:10 -0400

    Update MySQL.1.DDL.sql

--- datasets/MySQL/MySQL.1.DDL.sql
+++ datasets/MySQL/MySQL.1.DDL.sql
@@ -5,7 +5,7 @@ USE ecommerce;
 
 DROP TABLE IF EXISTS `customers`;
 CREATE TABLE `customers` (
-  `customer_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Uniquely identifies the Customer',
+  `customer_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Uniquely identifies the Customer ',
   `first_name` varchar(128) NOT NULL DEFAULT '' COMMENT 'First name of the customer',
   `last_name` varchar(128) NOT NULL DEFAULT '' COMMENT 'Last name of the customer',
   `email` varchar(128) NOT NULL DEFAULT '' COMMENT 'Email of the customer',
